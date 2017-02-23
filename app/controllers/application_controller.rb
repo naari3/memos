46bc8fa807a6c4329f045b16fd24e3f0796eeb13
naari3 <qazaaza98@gmail.com>
2017-02-23 15:52:23 +0900

    better current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
-
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
 
 private
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
   end
 
   def logged_in?
-    !!current_user
+    current_user.present?
   end
 end
