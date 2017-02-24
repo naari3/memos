8916aaeec153787851a506f5b1bc8159ad991aa1
naari3 <qazaaza98@gmail.com>
2017-02-24 10:39:15 +0900

    wip render from md to html

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,3 +1,13 @@
 class Memo < ApplicationRecord
+  require 'redcarpet'
   belongs_to :user
+
+  def markdown_render
+    unless @markdown
+      renderer = Redcarpet::Render::HTML.new
+      @markdown = Redcarpet::Markdown.new(renderer)
+    end
+
+    @markdown_render ||= @markdown.render(body)
+  end
 end
