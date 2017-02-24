3f0923f398b28ac32595332337be794ae91cde82
naari3 <qazaqaza98@gmail.com>
2017-02-24 11:44:39 +0900

    about xss

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,12 +2,14 @@ class Memo < ApplicationRecord
   require 'redcarpet'
   belongs_to :user
 
-  def markdown_render
+  def markdown_rendered
     unless @markdown
-      renderer = Redcarpet::Render::HTML.new
-      @markdown = Redcarpet::Markdown.new(renderer)
+      renderer = Redcarpet::Render::HTML.new(filter_html: true)
+      @markdown = Redcarpet::Markdown.new(renderer, {
+        autolink: true
+      })
     end
 
-    @markdown_render ||= @markdown.render(body)
+    @markdown_rendered ||= @markdown.render(body)
   end
 end
